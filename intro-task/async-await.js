6fe1dfe3132d8c17d65fe6d8933284a8c3070e01
Mehmet ali Çakmak <79807829+mehmet5643@users.noreply.github.com>
2022-10-26 13:43:12 +0300

    node_modules directory added main branch

--- intro-task/async-await.js
+++ intro-task/async-await.js
@@ -2,9 +2,9 @@ import axios from 'axios';
 
 export default async(number) =>{
     try {
-        const response = await axios.get(`https://jsonplaceholder.typicode.com/users/${number}`);
-        const post= await axios.get(`https://jsonplaceholder.typicode.com/posts?userId=${number}`);
-        return {response, post};
+        const {data:user} = await axios.get(`https://jsonplaceholder.typicode.com/users/${number}`);
+        const {data:post}= await axios.get(`https://jsonplaceholder.typicode.com/posts?userId=${number}`);
+        return {user, post};
         }
     catch (error) {
         return error;
