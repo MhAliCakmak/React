6254c2988d579c53a39eb0f47bf664ee3559781e
Mehmet ali Çakmak <79807829+mehmet5643@users.noreply.github.com>
2022-11-10 17:13:04 +0300

    added fetching directory

---
+++ fetching/src/App.js
@@ -0,0 +1,14 @@
+import User from './components/User'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Fetching Data</h1>
+      <br/>
+      <User/>
+    </div>
+  );
+}
+
+export default App;
