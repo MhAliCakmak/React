da1fb4e083ee4d965cb1445c1bac833f2d4f3147
Mehmet ali Çakmak <79807829+mehmet5643@users.noreply.github.com>
2022-11-15 13:11:24 +0300

    use Context using

--- context-api/src/components/Header.js
+++ context-api/src/components/Header.js
@@ -5,16 +5,11 @@ import React from 'react'
 export default function Header() {
     const theme = useContext(ThemeContext);
     
-    const themeStyle = {
-        backgroundColor: theme.background,
-        color: theme.foreground,
-        padding: "10px",
-        textAlign: "center"
-    };
+    
 
   return (
     <div>
-      <h1 style={themeStyle}>Context API Theme</h1>
+      <button style={{background: theme.background, color: theme.foreground}}>Change Theme</button>
     </div>
   )
 }
